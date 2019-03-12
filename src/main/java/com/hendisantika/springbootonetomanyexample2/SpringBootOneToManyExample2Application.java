6e817b556795176c32175c5f926d9b125e37491f
hendisantika <hendisantika@yahoo.co.id>
2019-03-12 21:59:30 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootonetomanyexample2/SpringBootOneToManyExample2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootonetomanyexample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOneToManyExample2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOneToManyExample2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootonetomanyexample2/SpringBootOneToManyExample2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootonetomanyexample2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootOneToManyExample2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
